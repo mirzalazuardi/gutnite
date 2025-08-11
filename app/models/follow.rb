76773fb26dabc5dd2dba3d9623e0e615b3204eee
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-11 13:48:46 +0700

    add validation cannot selffollow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -5,6 +5,9 @@ class Follow < ApplicationRecord
   validates :follower_id, uniqueness: { scope: :followed_user_id, message: "You are already following this user" }
   validates :followed_user_id, presence: true
   validates :follower_id, presence: true
+  validate :cannot_follow_self
 
-  # Additional validations or methods can be added here if needed
+  def cannot_follow_self
+    errors.add(:followed_user_id, "You cannot follow yourself") if follower_id == followed_user_id
+  end
 end
