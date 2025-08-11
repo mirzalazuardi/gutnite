450f6cfcc83cb047e8d9c894a4b2b95363cd88fe
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-11 11:49:30 +0700

    add validation and associations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,10 @@
 class Follow < ApplicationRecord
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed_user, class_name: 'User'
+
+  validates :follower_id, uniqueness: { scope: :followed_user_id, message: "You are already following this user" }
+  validates :followed_user_id, presence: true
+  validates :follower_id, presence: true
+
+  # Additional validations or methods can be added here if needed
 end
