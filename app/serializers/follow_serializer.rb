849989ca9d7614a7dbfd9784aabd350f9ce0d6d0
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-12 06:24:47 +0700

    add columns on serializer follow

--- app/serializers/follow_serializer.rb
+++ app/serializers/follow_serializer.rb
@@ -1,4 +1,10 @@
 class FollowSerializer
   include Alba::Resource
-  attributes :id, :follower_id, :followed_id, :created_at
+  attributes :id, :follower_id, :followed_user_id, :created_at
+  attribute :follower_name do |object|
+    object.follower.name
+  end
+  attribute :followe_user_name do |object|
+    object.followed_user.name
+  end
 end
