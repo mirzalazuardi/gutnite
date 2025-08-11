ade85c6f4ac96b6a24d0723a069e08dd739e9880
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-11 15:09:29 +0700

    add exception for association not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::AssociationNotFoundError do |exception|
+    render json: { error: "Association not found: #{exception.message}" }, status: :not_found
+  end
 end
