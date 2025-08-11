321ccb449caa93e7f6290ab17db983d580bca72c
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-11 23:55:22 +0700

    cut unused controller

--- app/controllers/api/v1/users/following_sleep_records_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::Users::FollowingSleepRecordsController < Api::V1::UsersController
-
-  def index
-    @sleep_records = @user.following_sleep_records.includes(:user)
-    render json: @sleep_records, each_serializer: SleepRecordSerializer, status: :ok
-  end
-end
