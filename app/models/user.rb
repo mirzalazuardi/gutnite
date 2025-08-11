2e29be802062ae89bab4b27352171437215f37c9
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-11 13:53:17 +0700

    add assoc and validation to model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :follows, foreign_key: :follower_id, dependent: :destroy
+  has_many :followings, through: :follows, source: :followed_user
+
+  has_many :inverse_follows, class_name: 'Follow', foreign_key: :followed_user_id, dependent: :destroy
+  has_many :followers, through: :inverse_follows, source: :follower
+
+  validates :name, presence: true, uniqueness: true
 end
