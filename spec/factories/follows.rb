307d9dd9bd6a1d32cb42b26ab3b9bf6a23671512
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-12 08:09:43 +0700

    fix factories. add traits

--- spec/factories/follows.rb
+++ spec/factories/follows.rb
@@ -1,6 +1,21 @@
 FactoryBot.define do
   factory :follow do
-    follower { "MyString" }
-    followed_user { "MyString" }
+    follower { User.first }
+  end
+
+  trait :with_follower_first do
+    follower { User.first || create(:user) }
+  end
+
+  trait :with_follower_last do
+    follower { User.last }
+  end
+
+  trait :with_followed_first do
+    followed_user { User.first || create(:user) }
+  end
+
+  trait :with_followed_last do
+    followed_user { User.last }
   end
 end
--- spec/factories/sleep_records.rb
+++ spec/factories/sleep_records.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
   factory :sleep_record do
-    went_to_bed { "2025-08-08 00:18:32" }
+    went_to_bed_at { "2025-08-08 00:18:32" }
     woke_up_at { "2025-08-08 00:18:32" }
+  end
+
+  trait :with_to_bed_at_a_week_ago_random do
+    went_to_bed_at { 1.week.ago.beginning_of_day + 20.hours + rand(3).hours }
+  end
+
+  trait :with_to_bed_at_a_week_ago do
+    went_to_bed_at { 1.week.ago.beginning_of_day }
   end
 end
