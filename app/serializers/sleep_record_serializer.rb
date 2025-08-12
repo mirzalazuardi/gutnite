bd7c57b579a404376c6e7ebd319a3f673b4f3364
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-12 13:08:05 +0700

    add trait to follow serializer

--- app/serializers/sleep_record_serializer.rb
+++ app/serializers/sleep_record_serializer.rb
@@ -1,4 +1,12 @@
 class SleepRecordSerializer
   include Alba::Resource
   attributes :id, :went_to_bed_at, :woke_up_at, :duration, :created_at, :updated_at
+  trait :user do
+    attribute :user do |record|
+      {
+        id: record.user.id,
+        name: record.user.name
+      }
+    end
+  end
 end
