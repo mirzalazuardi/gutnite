ed45e7bf0700eb925e324cad9ae6f4dcc2786248
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-14 09:19:34 +0700

    add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { should have_many(:follows).with_foreign_key(:follower_id) }
+    it { should have_many(:followings).through(:follows).source(:followed_user) }
+    it { should have_many(:inverse_follows).class_name('Follow').with_foreign_key(:followed_user_id) }
+    it { should have_many(:followers).through(:inverse_follows).source(:follower) }
+  end
+
+  context 'validations' do
+    it 'is valid with a unique name' do
+      user1 = User.create(name: "UniqueUser")
+      user2 = User.new(name: "UniqueUser")
+      expect(user2).not_to be_valid
+      expect(user2.errors[:name]).to include("has already been taken")
+    end
+  end
 end
