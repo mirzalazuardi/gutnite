bc55b732dd64a8822f7964878ec3e8875b965b89
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-14 07:46:33 +0700

    add pagy backend and method render_list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::API
+  include Pagy::Backend
+
   rescue_from ActiveRecord::AssociationNotFoundError do |exception|
     render json: { error: "Association not found: #{exception.message}" }, status: :not_found
+  end
+
+  def render_list(source, serializer, opts = {}, opt_serializer = {})
+    if source.empty?
+      render json: [], status: :ok
+    else
+      pagy, records = QueryCacheService.new(source, **opts).call
+      pagy_headers_merge(pagy)
+      render json: serializer.new(records, **opt_serializer).serialize, status: :ok
+    end
   end
 end
