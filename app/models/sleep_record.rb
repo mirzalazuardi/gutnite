4eeaf305dc8d02f882e5e821fdb7b8c586ee666d
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2025-08-14 09:28:36 +0700

    add spec for model sleep record

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,9 +1,17 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
+  validates :user, :went_to_bed_at, :woke_up_at, presence: true
 
   before_validation :calculate_duration
+  validate :went_to_bed_at_before_woke_up_at
 
   def calculate_duration
     self.duration = (woke_up_at - went_to_bed_at) / 3600.0 if went_to_bed_at && woke_up_at
+  end
+
+  def went_to_bed_at_before_woke_up_at
+    if went_to_bed_at && woke_up_at && went_to_bed_at >= woke_up_at
+      errors.add(:base, "Went to bed must be before Woke up at")
+    end
   end
 end
--- spec/models/sleep_record_spec.rb
+++ spec/models/sleep_record_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe SleepRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  context 'validations' do
+    let(:user) { create(:user) }
+    let(:sleep_record) { SleepRecord.new(user: user, went_to_bed_at: Time.current, woke_up_at: Time.current + 8.hours) }
+
+    it 'is valid with valid attributes' do
+      expect(sleep_record).to be_valid
+    end
+
+    it 'is not valid without a user' do
+      sleep_record.user = nil
+      expect(sleep_record).not_to be_valid
+      expect(sleep_record.errors[:user]).to include("must exist")
+    end
+
+    it 'is not valid without a went to bed at' do
+      sleep_record.went_to_bed_at = nil
+      expect(sleep_record).not_to be_valid
+      expect(sleep_record.errors[:went_to_bed_at]).to include("can't be blank")
+    end
+
+    it 'is not valid without an woke up at' do
+      sleep_record.woke_up_at = nil
+      expect(sleep_record).not_to be_valid
+      expect(sleep_record.errors[:woke_up_at]).to include("can't be blank")
+    end
+
+    it 'is not valid if start time is after end time' do
+      sleep_record.went_to_bed_at = Time.current + 1.hour
+      sleep_record.woke_up_at = Time.current
+      expect(sleep_record).not_to be_valid
+      expect(sleep_record.errors[:base]).to include("Went to bed must be before Woke up at")
+    end
+  end
 end
